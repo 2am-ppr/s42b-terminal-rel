918942aa79061a5ff40a11966df853a2f3d641d0
Igor Yeremin <igor.yeremin@gmail.com>
2020-10-30 18:21:45 -0400

    Excel equivalent graph done

--- S42B-terminal/frmMain.cs
+++ S42B-terminal/frmMain.cs
@@ -13,6 +13,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OxyPlot;
+using OxyPlot.Axes;
 using OxyPlot.Series;
 
 namespace S42B_terminal
@@ -196,10 +197,12 @@ namespace S42B_terminal
 
 									serialPortDriver.Write(packet, 0, packet.Length);
 
-									BeginInvoke(new Action(() =>
-									{
-										refreshChart();
-									}));
+									Task.Delay(TimeSpan.FromSeconds(1))
+										.ContinueWith(new Action<Task>((t) =>
+										{
+											refreshChart();
+										}));
+
 								}
 							}
 						}
@@ -235,6 +238,18 @@ namespace S42B_terminal
 				Title = "PID Tune"
 			};
 
+			var startPos = pointLog.First().PosTarget;
+
+			var header = pointLog.TakeWhile(x => Math.Abs(x.PosError) < 6 && x.PosTarget == startPos).Count();
+			pointLog.RemoveRange(0, header);
+
+			var endPos = pointLog.Last().PosTarget;
+			var tail = pointLog.Reverse<TestPoint>().TakeWhile(x => Math.Abs(x.PosError) < 6 && x.PosTarget == endPos).Count();
+			pointLog.RemoveRange(pointLog.Count - tail, tail);
+
+
+			var colors = new[] { "0072bd", "d95319", "edb120", "7e2f8e", "77ac30", "4dbeee", "a2142f" };
+
 			foreach (var field in new[] { "PosMeasured", "PosTarget", "PosError", "VelMeasured", "VelTarget", "VelError", "PidI" })
 			{
 
@@ -248,8 +263,54 @@ namespace S42B_terminal
 
 				model.Series.Add(series);
 			}
+			(model.Series[0] as LineSeries).YAxisKey = "position";
+			(model.Series[1] as LineSeries).YAxisKey = "position";
+			(model.Series[2] as LineSeries).YAxisKey = "posError";
+			(model.Series[5] as LineSeries).YAxisKey = "velError";
 
-			plotViewPID.Model = model;
+			for (int i = 0; i < model.Series.Count; i++)
+			{
+				var s = model.Series[i] as LineSeries;
+				var colorValue = UInt32.Parse(colors[i], System.Globalization.NumberStyles.HexNumber) | 0xff000000u;
+				var color = OxyColor.FromUInt32(colorValue);
+				var lightColor = OxyColor.FromUInt32(colorValue & 0x7fffffffu);
+
+				if (s.DataFieldY.EndsWith("Target"))
+				{
+
+					//s.MarkerType = MarkerType.Cross;
+					//s.MarkerFill = OxyColors.Transparent;
+					//s.MarkerStroke = OxyColors.Automatic;
+					//s.LineStyle = LineStyle.None;
+					//s.Color = OxyColors.Transparent;
+					s.Color = color;
+					s.LineStyle = LineStyle.Dot;
+				} else
+				{
+					s.Color = color;
+				}
+			}
+
+			model.Axes.Add(new LinearAxis() { Position = AxisPosition.Right, Title = "Derivative units", Key = "default", EndPosition = 0.45 });
+			model.Axes.Add(new LinearAxis() { Position = AxisPosition.Right, Title = "Position", Key = "position", StartPosition = 0.55 });
+			model.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, Title = "Position Error", Key = "posError", StartPosition = 0.55,
+				MajorGridlineStyle = LineStyle.Automatic,
+				Maximum = 300,
+				Minimum = -300,
+				MinorGridlineStyle = LineStyle.Dot,
+				MajorStep = 100
+			});
+			model.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, Title = "Velocity units", Key = "velError", EndPosition = 0.45,
+				MajorGridlineStyle = LineStyle.Automatic,
+				Maximum = 3000,
+				Minimum = -3000,
+				MinorGridlineStyle = LineStyle.Dot,
+				MajorStep = 1000
+			});
+
+			Invoke(new Action(() => {
+				plotViewPID.Model = model;
+			}));
 		}
 
 
